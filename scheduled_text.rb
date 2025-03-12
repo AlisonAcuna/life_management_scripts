ec77eae6f64723e5781d37b2868910c293bf22c7
alison-acuna <aacuna136@gmail.com>
2025-03-12 08:37:12 -0700

    Project Outline

---
+++ scheduled_text.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+# requires
+
+# includes
+
+def send_schedule_text(msg, from, to_array, time)
+  # send msg to numbers at time
+  # open question: How can I send messages from script
+  # open question: How to trigger scipt at time?  Does it need to run continuously?
+  # future optimization: normalize time
+  # future optimization: Directory so can enter names rather than numbers
+end
+
+main()
