0bec93393ca7b4be94df560afe77920fcb5c6e63
alison-acuna <aacuna136@gmail.com>
2025-05-29 08:45:04 -0700

    length_of_longest_substring

--- practice_problems.rb
+++ practice_problems.rb
@@ -16,3 +16,24 @@ def two_sum(nums, target)
         end
     end
 end
+
+def length_of_longest_substring(s)
+    substrings = []
+    substring_hash = length_of_substring(s, substrings)
+    substrings.max_by(&:length).length
+end
+
+def length_of_substring(s, substrings)
+    return substrings if s.nil?
+
+    answer = ""
+    s.chars.each do |c|
+        break if answer.include?(c)
+        answer << c
+    end
+
+    substrings << answer
+    s = s[1..]
+
+    length_of_substring(s, substrings)
+end
